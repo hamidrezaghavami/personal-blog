8e6fdd2804563604fb32a5546287e111d7a40bc8
Hamid Reza Ghavami <hamidr.ghavami@gmail.com>
2025-10-03 22:49:36 +0300

    first changes

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from express;
+import { Router, adminRouter } from "./userRouters.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/', Router);
+app.use('/', adminRouter);
+
+// Server initialisation: http://localhost:3000
+app.listen(PORT, () => {
+    console.log(`Server is running on the port ${PORT}`)
+});
---
+++ userRouters.js
@@ -0,0 +1,80 @@
+import express from 'express';
+
+const Router = express.Router();
+const adminRouter = express.Router();
+
+
+//Home router
+Router.get('/', (req, res) => {
+    res.send('Welcome to my Personal Blog');
+});
+
+
+// About router
+Router.get('/about', (req, res) => { 
+    res.send("<h1>About</h1><p>This is a simple personal blog project.</p>");
+});
+
+// middleware for authentication for admin routers
+Router.use((req, res, next) => { 
+    const isLoggedIn = true;
+    if (!isLoggedIn) res.status(401).send("Not authorized");
+    next();
+});
+
+// Admin DashBoard
+adminRouter.get('/admin', (req, res) => { 
+    res.send('Admin Dashboard');
+});
+
+// create new article
+adminRouter.get('/new', (req, res) => { 
+    console.log("newArticle");
+});
+
+adminRouter.post('/new', (req, res) => {
+    const { title, content } = req.body;
+    
+    const newArticle = {
+        id: Date.now(),
+        title, 
+        content, 
+        date: new Date().toISOString()
+    };
+    
+    console.log("new article created: ", newArticle);
+    
+    res.redirect("/admin"); // back to dashboard after save
+});
+
+// edit article
+adminRouter.get('/edit/:id', (req, res) => { 
+    const { id } = req.params;
+    
+    res.redirect(`edit form from Article with ID: ${ id }`);
+});
+
+adminRouter.post('/edit/:id', (req, res) => { 
+    const { id } = req.params;
+    const { title, content } = req.body;
+
+    const updateArticle = {
+        id,
+        title, 
+        content, 
+        date: new Date()
+    }
+
+    console.log("Article updated: ", updateArticle);
+    res.redirect("/admin"); // back to dashboard
+});
+
+adminRouter.post('/delete/:id', (req, res) => { 
+    const { id } = req.params;
+
+    console.log(`Article deleted: ${id}`);
+
+    res.redirect("/admin"); // back to dashboard 
+});
+
+export { Router, adminRouter};
